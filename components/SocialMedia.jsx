afafc2c7e9b63b26df09f31eb4c21d4eb2ad6032
SajalSatsangi <119046714+SajalSatsangi@users.noreply.github.com>
2023-06-20 23:18:28 +0530

    Add files via upload

---
+++ components/SocialMedia.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { BsTwitter, BsInstagram } from 'react-icons/bs';
+import { FaFacebookF } from 'react-icons/fa';
+
+const SocialMedia = () => (
+  <div className="app__social">
+    <div>
+      <BsTwitter />
+    </div>
+    <div>
+      <FaFacebookF />
+    </div>
+    <div>
+      <BsInstagram />
+    </div>
+  </div>
+);
+
+export default SocialMedia;
